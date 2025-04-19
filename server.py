9dba4e363d9be6102d947dd36f738db5347d9377
shinoairisu <37396293+shinoairisu@users.noreply.github.com>
2025-04-20 01:32:53 +0800

    Update server.py

--- server.py
+++ server.py
@@ -8,41 +8,39 @@ from loguru import logger
 from fastapi.responses import HTMLResponse
 from fastapi_socketio import SocketManager
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI,Cookie, Response, File, Form, UploadFile
+from fastapi import FastAPI, Cookie, Response, File, Form, UploadFile
 
+from utils.dogs import re_build_project, observer
 
-
-
-from utils.dogs import re_build_project,observer
-
-parser = argparse.ArgumentParser(description='前端服务器')
-parser.add_argument('-p','--port', type=int,default=12580,help='服务器端口')
-parser.add_argument('-s','--host', type=str,default='0.0.0.0', help='服务器地址')
-parser.add_argument('-d','--debug', type=bool,default=True, help='是否开启debug模式')
+parser = argparse.ArgumentParser(description="前端服务器")
+parser.add_argument("-p", "--port", type=int, default=12580, help="服务器端口")
+parser.add_argument("-s", "--host", type=str, default="0.0.0.0", help="服务器地址")
+parser.add_argument("-d", "--debug", type=bool, default=True, help="是否开启debug模式")
 
 args = parser.parse_args()
-re_build_project() # 启动时会强制重构项目
+re_build_project()  # 启动时会强制重构项目
 
 if args.debug:
-    observer.start() # 监控前端文件改变
+    observer.start()  # 监控前端文件改变
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-socket_manager = SocketManager(app=app,mount_location="/wsk",socketio_path="/wsk/socket.io") 
+socket_manager = SocketManager(
+    app=app, mount_location="/wsk", socketio_path="/wsk/socket.io"
+)
 # socketio默认挂载点是/ws。新版的fastapi(0.115.12)中socketio_path正确应当是 '/ws/socket.io'
 # socketio_path必须是 挂载点mount_location加上具体自定义路径。比如此处的/wsk/socket.io,否则和前端对不齐。
 # 因为fastapi的特殊,namesapce必须得写，否则找不到发送端点。
 
 
-
 # 主路由
 # html的页面路由由本函数实现，不可删除，只可以修改。
-@app.get('/content/{html_name}/{route:path}')
-async def static_pages(html_name:str,route:str):
+@app.get("/content/{html_name}/{route:path}")
+async def static_pages(html_name: str, route: str):
     """支持静态路由，如 http://127.0.0.1:12580/content/go//route1/route2/route3..."""
     if not os.path.exists(f"static/{html_name}.html"):
         return HTMLResponse(content="404 Not found!")
-    async with aiofiles.open(f"static/{html_name}.html", 'r', encoding='utf-8') as f:
+    async with aiofiles.open(f"static/{html_name}.html", "r", encoding="utf-8") as f:
         return HTMLResponse(content=await f.read())
 
 
@@ -50,45 +48,50 @@ async def static_pages(html_name:str,route:str):
 # 前端要用的是服务端事件。也可以用自定义事件。
 # 内置事件https://blog.csdn.net/tiantangyouzui/article/details/51791129
 # socketio 模板，不用可以删除
-@socket_manager.on('connect',namespace='/ws')
-async def handle_connect(sid,message):
-    logger.debug("前端连接成功，sid为{}",sid)
-    await socket_manager.emit('test', '你好，前端！',namespace="/ws") # 必须要写namespace
+@socket_manager.on("connect", namespace="/ws")
+async def handle_connect(sid, message):
+    logger.debug("前端连接成功，sid为{}", sid)
+    await socket_manager.emit(
+        "test", "你好，前端！", namespace="/ws"
+    )  # 必须要写namespace
 
 
 # axios的get请求模板，不用可以删除
-@app.get('/api/getest')
-async def getest(name:str):
+@app.get("/api/getest")
+async def getest(name: str):
     """一个前端的get请求支持系统"""
-    logger.debug("前端发送测试请求，内容为{}",name)
+    logger.debug("前端发送测试请求，内容为{}", name)
     return "你好，前端！"
 
+
 # axios的cookies获取模板，不用可以删除
-@app.get('/api/cookiegetest')
+@app.get("/api/cookiegetest")
 async def cookieget(name: str | None = Cookie(default=None)):
     """测试接收cookies"""
-    logger.debug("前端发送带cookies请求，内容为{}",name)
+    logger.debug("前端发送带cookies请求，内容为{}", name)
     return {"message": "cookie测试成功", "cookie_key": "ads_id", "cookie_value": name}
 
+
 # axios的cookies设置模板，不用可以删除
-@app.get('/api/cookiesetest')
-async def cookieset(response:Response):
+@app.get("/api/cookiesetest")
+async def cookieset(response: Response):
     """测试设置cookies"""
     logger.debug("收到cookies设置请求")
-    response.set_cookie(key="ads_id", value="1234567890", expires=time.time()+3600)
+    response.set_cookie(key="ads_id", value="1234567890", expires=time.time() + 3600)
     return {"message": "cookie设置成功"}
+
 
 # 可在一个路径操作中声明多个 File 与 Form 参数，
 # 但不能同时声明要接收 JSON 的 Body 字段。
 # 因为此时请求体的编码为 multipart/form-data，不是 application/json。
 # axios的文件上传模板，不用可以删除
-@app.post('/api/upload')
-async def upload(file:bytes = File(...,max_length=20097152)): 
+@app.post("/api/upload")
+async def upload(file: bytes = File(..., max_length=20097152)):
     # 大于20M的不建议用这个方式，推荐用UploadFile。
     # 20,097,152 = 20M ,超了会报错
     # 在使用File模式下，这个参数名字必须叫file。不管表单里是什么。
-    logger.debug("收到文件上传请求,文件大小为{}",len(file))
+    logger.debug("收到文件上传请求,文件大小为{}", len(file))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, host=args.host, port=args.port)
